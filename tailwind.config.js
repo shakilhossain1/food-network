3509cd83b91987a1114f89610705a27f67ae9cea
Shakil Hossain <sh9743441@gmail.com>
2021-11-03 14:52:57 +0600

    complere

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,23 @@ module.exports = {
   purge: ['./*.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Montserrat', 'sans-serif'],
+    },
+    extend: {
+      boxShadow: {
+        pimary: '0px 0px 40px rgba(0, 0, 0, 0.15)'
+      },
+      colors: {
+        orange: {
+          400: '#FE492D',
+          600: '#FE482D',
+        },
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
